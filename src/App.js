97a6b1b58ccb581f464b2a0bd5f4c04417244d5a
Garret Saarinen <garretbsaarinen@gmail.com>
2018-01-24 21:39:13 -0600

    fixing hard-coded redirect url

--- src/App.js
+++ src/App.js
@@ -160,10 +160,13 @@ class App extends Component {
           {playlistsToRender.map(playlist =>
             <Playlist playlist={playlist} />
           )}
-        </div> : <button onClick={() =>window.location = 'http://localhost:8888/login'}
-        style={{padding: '20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
+        </div> : <button onClick={() =>{
+          window.location = window.location.includes('localhost')
+          ? 'http://localhost:8888/login'
+          : 'https://gs-spotify-better-playlists.herokuapp.com/login' }
         }
-
+        style={{padding: '20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
+      }
       </div>
     );
   }
